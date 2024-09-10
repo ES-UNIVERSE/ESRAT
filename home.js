09ba044776a0148953b64cb4f454601797b9986c
Easy Spark <esplaysbgmi@gmail.com>
2024-09-10 09:59:33 +0530

    updated home screen

--- home.js
+++ home.js
@@ -5,3 +5,16 @@ document.addEventListener('mousemove', (event) => {
     cursorShadow.style.left = event.clientX + 'px';
     cursorShadow.style.top = event.clientY + 'px';
 });
+
+window.addEventListener('scroll', () => {
+    const scrollTexts = document.querySelectorAll('.scroll-text');
+    const triggerPoint = window.innerHeight / 1.2;
+  
+    scrollTexts.forEach(text => {
+      const textTop = text.getBoundingClientRect().top;
+  
+      if (textTop < triggerPoint) {
+        text.classList.add('show', 'no-glow'); // Add 'show' and 'no-glow' classes when scrolled into view
+      }
+    });
+  });
