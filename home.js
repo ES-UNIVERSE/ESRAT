1bff185484b6051ee74f874958098ed8413d58ea
Easy Spark <esplaysbgmi@gmail.com>
2024-09-10 14:57:48 +0530

    updated

--- home.js
+++ home.js
@@ -18,3 +18,17 @@ window.addEventListener('scroll', () => {
       }
     });
   });
+
+  const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
